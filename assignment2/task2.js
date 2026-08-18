5d401805e73e7b65ba7dca1377e5f936484552a0
Abdeali Makda <abdealimac@gmail.com>
2026-08-18 12:47:28 +0530

    assignment 2

---
+++ assignment2/task2.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+    res.end(`
+        <h1>Student Portal</h1>
+        <p>Name: John Doe</p>
+        <p>Course: Full Stack Development</p>
+        <p>College: XYZ College</p>
+        <p>Welcome to our Node.js application.</p>
+    `);
+
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
